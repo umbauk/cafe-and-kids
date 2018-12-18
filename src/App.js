5816e32cf8e8973979297c3c876f22b569ec0cc4
Darren Greenfield <darren.greenfield@gmail.com>
2018-12-18 22:11:56 +0000

    Current Location working

--- src/App.js
+++ src/App.js
@@ -1,28 +1,82 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+/* global google */
+
+
+function loadJS(src) {
+  var ref = window.document.getElementsByTagName("script")[0];
+  var script = window.document.createElement("script");
+  script.src = src;
+  script.async = true;
+  ref.parentNode.insertBefore(script, ref);
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      center: {
+        lat: 37.774929,
+        lng: -122.419416
+      }
+    }
+
+    this.initMap = this.initMap.bind(this)
+    this.getCurrentLocation = this.getCurrentLocation.bind(this)
+  }
+
+  componentDidMount() {
+    // Connect the initMap() function within this class to the global window context,
+    // so Google Maps can invoke it
+    window.initMap = this.initMap;
+    // Asynchronously load the Google Maps script, passing in the callback reference
+    loadJS('https://maps.googleapis.com/maps/api/js?key=AIzaSyBoKmshPxsNC3n5M88_BKq2I_IJgiVx47g&libraries=places&callback=initMap')
+  }
+
+  initMap() {
+    const zoom = 14
+    let map
+
+    this.getCurrentLocation()
+      .then((userCoords) => { 
+        let mapConfig = {
+          center: userCoords,
+          zoom
+        } 
+        map = new google.maps.Map(this.mapElement, mapConfig)
+      })
+      .catch((error) => { console.log(error) })
+  
+  }
+
+  getCurrentLocation() {
+    return new Promise((resolve, reject) => {
+      if (!navigator.geolocation) reject(new Error('Get User Location error'))
+      else {
+        return Promise.resolve(navigator.geolocation.getCurrentPosition((position) => {
+          let currentCoordinates = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          }
+          this.setState({ center: currentCoordinates })
+          console.log(`pos: ${JSON.stringify(currentCoordinates)}`)
+          resolve(currentCoordinates)
+        }))
+      }
+        //this.recenterMap()
+        //this.refreshNearbyPlaces()
+    
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+            <div ref={mapElement => (this.mapElement = mapElement) } style={{ height: '500px', width: '500px' }}/>
+     
+    )
   }
 }
 
-export default App;
+export default App
